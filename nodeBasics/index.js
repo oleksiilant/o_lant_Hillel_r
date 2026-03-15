46fee23ea1372b855c7c699a8eeff4d3ce1e8e6b
Oleksi Lant <leshalan@gmail.com>
2026-03-15 15:14:48 +0100

    Add chalk colored console output

---
+++ nodeBasics/index.js
@@ -0,0 +1,7 @@
+import chalk from 'chalk';
+
+console.log(chalk.red('Hello everybody'));
+console.log(chalk.green('-----------'));
+console.log(chalk.blue('Slava'));
+console.log(chalk.yellow('Ukraine'));
+console.log(chalk.green('-----------'));
