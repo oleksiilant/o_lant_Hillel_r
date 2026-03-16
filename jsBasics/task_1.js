97f5247838298bf0449c234399f5cea26434e076
Oleksi Lant <leshalan@gmail.com>
2026-03-16 23:23:32 +0100

    added homework No.3

---
+++ jsBasics/task_1.js
@@ -0,0 +1,20 @@
+//Завдання 1: Оголошення змінних для примітивних типів
+
+//рядок, число, буль, null та undefined
+
+const user = "Oleksi Lantukh";
+const age = 33;
+
+const entrepreneur = true;
+const retired = age < 65 && entrepreneur;
+
+const hasPlane = null;
+const nullExample = entrepreneur && hasPlane;
+
+let Lviv; 
+
+console.log(user);
+console.log(age);
+console.log(retired);
+console.log(nullExample);
+console.log(Lviv);
