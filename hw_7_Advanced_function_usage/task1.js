0f42d49306a5a746914f7ae262d28365fe9c4e1a
Oleksi Lant <leshalan@gmail.com>
2026-03-28 15:59:01 +0100

    created folder and files

---
+++ hw_7_Advanced_function_usage/task1.js
@@ -0,0 +1,22 @@
+// 1.Створіть функцію handleNum яка буде приймати 3 параметри.
+// число
+// Колбек функцію яку треба викликати якщо передане число парне
+// Колбек функцію яку треба викликати якщо передане число непарне
+// 2. Створіть ще дві функції які ви будете передавати у якості колбеків, 
+// наприклад handleEven та handleOdd. Кожна з них має виводити просте повідомлення в консоль.
+// Наприклад handleEven буде виводити текст “number is even”, 
+// a handleOdd буде виводити текст “number is odd”
+// 3. Викличте функцію handleNum і передайте в якості аргументів довільне число 
+// і дві функції які ви створили раніше
+
+function handleNum(number, perdaneParne, peredaneNeParne) {
+    return 
+}
+function handleEven(){
+    console.log("number is even");
+}
+function handleOdd(){
+    console.log("number is odd");
+}
+
+handleNum(5, )
