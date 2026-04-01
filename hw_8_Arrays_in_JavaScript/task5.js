a0b393eea671b7edddd40eb8170f6d66a30b4e34
Oleksi Lant <leshalan@gmail.com>
2026-04-01 23:35:23 +0200

    added solution for the 5th task

--- hw_8_Arrays_in_JavaScript/task5.js
+++ hw_8_Arrays_in_JavaScript/task5.js
@@ -5,3 +5,10 @@
 // Створіть новий масив, який містить всі елементи з обох вихідних масивів.
 // Виведіть отриманий об'єднаний масив на консоль
 
+const firstArray = [1, 3, 5, 7, 9];
+const secondArray = [2, 4, 6, 8, 10];
+const newMassive = firstArray.concat(secondArray);
+
+console.log(newMassive);
+console.log(newMassive.join(", "))
+//console.log(newMassive.sort)
