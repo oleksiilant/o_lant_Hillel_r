b71fecf4ec1c7566e1786fcff19e389282939311
Oleksi Lant <leshalan@gmail.com>
2026-04-01 21:48:28 +0200

    added solution for the 1st task

---
+++ hw_8_Arrays_in_JavaScript/example.js
@@ -0,0 +1,9 @@
+const myArray = [1,2,3,4];
+let num = 1;
+let num2 = num;
+num++;
+console.log(num);
+
+console.log(myArray);
+myArray.push(5)
+console.log(myArray);
--- hw_8_Arrays_in_JavaScript/task1.js
+++ hw_8_Arrays_in_JavaScript/task1.js
@@ -14,3 +14,22 @@
 
 
 
+const number = [2, -5, 0, 7, -3, 0, 10, -8];
+let positiveCount = 0; 
+let negativeCount = 0; 
+let zeroCount = 0; 
+for (let i = 0; i < number.length; i++) {
+    if (number[i] > 0) {
+        positiveCount++;
+    } else if (number[i] < 0) {
+        negativeCount++
+    } else {
+        zeroCount++;
+    }
+}
+// console.log("Кількість позитивних:", positiveCount);
+// console.log("Кількість негативних:", negativeCount);
+// console.log("Кількість нулів:", zeroCount);
+console.log(`Кількість позитивних чисел: ${positiveCount}`);
+console.log(`Кількість негативних чисел: ${negativeCount}`);
+console.log(`Кількість нульових чисел: ${zeroCount}`);
