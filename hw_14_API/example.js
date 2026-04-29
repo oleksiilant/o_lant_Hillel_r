d80748ef24f36ae4e0114e2061ee14aff216d750
oleksiilant <leshalan@gmail.com>
2026-04-30 00:50:40 +0200

    fix: eslint config add jest/node globals, fix duplicate axios in example.js

--- hw_14_API/example.js
+++ hw_14_API/example.js
@@ -1,8 +1,55 @@
-// import axios from "axios";
+// // import axios from "axios";
+
+// // test("Check status", async () => {
+// //   const response = await axios.delete(
+// //     "https://jsonplaceholder.typicode.com/posts/1"
+// //   );
+
+// //   console.log(response.status);
+// //   console.log(response.data);
+
+// //   expect(response.status).toBe(200);
+// //   expect(response.data).toEqual({});
+// // });
+
+// // let response;
+
+// // beforeEach(() => {
+// //   response = { status: 200, value: "hello" };
+// // });
+
+// // test("Check status", () => {
+// //   expect(response.status).toBe(200);
+// // });
+
+// const axios = require("axios");
+
+// //import axios from "axios";
+
+// test("Check status", async () => {
+//   const response = await axios.post(
+//     "https://jsonplaceholder.typicode.com/posts",
+//     {
+//       title: "foo",
+//       body: "bar",
+//       userId: 1,
+//     },
+//     {
+//       headers: {
+//         "Content-type": "application/json; charset=UTF-8",
+//       },
+//     },
+//   );
+
+//   console.log(response.status);
+//   console.log(response.data);
+
+//   expect(response.status).toBe(201);
+// });
 
 // test("Check status", async () => {
 //   const response = await axios.delete(
-//     "https://jsonplaceholder.typicode.com/posts/1"
+//     "https://jsonplaceholder.typicode.com/posts/1",
 //   );
 
 //   console.log(response.status);
@@ -11,59 +58,3 @@
 //   expect(response.status).toBe(200);
 //   expect(response.data).toEqual({});
 // });
-
-
-
-// let response;
-
-// beforeEach(() => {
-//   response = { status: 200, value: "hello" };
-// });
-
-// test("Check status", () => {
-//   expect(response.status).toBe(200);
-// });
-
-
-
-const axios = require("axios");
-
-//import axios from "axios";
-
-test("Check status", async () => {
-  const response = await axios.post(
-    "https://jsonplaceholder.typicode.com/posts",
-    {
-      title: "foo",
-      body: "bar",
-      userId: 1,
-    },
-    {
-      headers: {
-        "Content-type": "application/json; charset=UTF-8",
-      },
-    }
-  );
-
-  console.log(response.status);
-  console.log(response.data);
-
-  expect(response.status).toBe(201);
-});
-
-
-const axios = require("axios");
-
-test("Check status", async () => {
-  const testTitle = "changed";
-
-  const response = await axios.delete(
-    "https://jsonplaceholder.typicode.com/posts/1"
-  );
-
-  console.log(response.status);
-  console.log(response.data);
-
-  expect(response.status).toBe(200);
-  expect(response.data).toEqual({});
-});
